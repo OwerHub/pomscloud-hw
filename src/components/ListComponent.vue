1e074124c2bdf39f6f562039b7e7243f99659228
OwerHub <owerwild@gmail.com>
2022-02-18 09:33:38 +0100

    Modal open, close OK

--- src/components/ListComponent.vue
+++ src/components/ListComponent.vue
@@ -1,15 +1,58 @@
 <template>
   <div>this is listComponent</div>
-  <div>{{ $store.state.test }}</div>
-  <ul>
-    <li v-for="user in $store.state.testUsers" v-bind:key="user.id">
+
+  <input type="text" v-model="search" placeholder="search blogs" />
+  <div>
+    <button @click="onlyActive = !onlyActive">
+      <span v-if="onlyActive">Show All</span>
+      <span v-else>Only Active</span>
+    </button>
+  </div>
+  <div>
+    <div
+      v-for="user in filteredUsers"
+      v-bind:key="user.id"
+      v-show="user.status === 'active' || (!onlyActive && user.status === 'passive')"
+    >
       {{ user.name }} | {{ user.email }} | {{ user.descript }} | {{ user.type }}
-    </li>
-  </ul>
+    </div>
+  </div>
+  <Modal @modalClose="closeModal" v-if="modalOpen" />
+  <button @click="modalOpen = !modalOpen">Modal Open</button>
 </template>
 
 <script>
+import Modal from "./Modal.vue";
+
 export default {
   name: "ListComponent",
+  components: {
+    Modal,
+  },
+  data() {
+    return {
+      userArray: [],
+      search: "",
+      onlyActive: false,
+      modalOpen: false,
+      userIdForModal: 0,
+    };
+  },
+  created() {
+    this.userArray = this.$store.state.testUsers;
+  },
+  computed: {
+    filteredUsers: function () {
+      return this.userArray.filter((user) => {
+        return user.name.match(this.search) || user.descript.match(this.search);
+      });
+    },
+  },
+  methods: {
+    closeModal: function () {
+      console.log("closamodal ");
+      this.modalOpen = false;
+    },
+  },
 };
 </script>
---
+++ src/components/Modal.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="modalOuter">
+    <div class="modalInner">
+      modalInner
+
+      <button @click="closeModal">close</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Modal",
+  methods: {
+    closeModal: function () {
+      this.$emit("modalClose");
+    },
+  },
+};
+</script>
+
+<style>
+.modalOuter {
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100vh;
+  width: 100vw;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.modalInner {
+  height: 70vh;
+  width: 50vw;
+  background-color: white;
+}
+</style>
